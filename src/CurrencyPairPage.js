dd0119ea459a855063c7f48065a83511e862c81b
Jacob <jacoboquinn4@gmail.com>
2023-08-28 14:23:50 -0500

    Completed currency pair page

--- src/CurrencyPairPage.js
+++ src/CurrencyPairPage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { getCurrencyPairInfo } from './api';
+import './CurrencyPairPage.css';
 
 function CurrencyPairPage() {
   const { pair } = useParams();
@@ -13,17 +14,23 @@ function CurrencyPairPage() {
   }, [pair]);
 
   return (
-    <div>
-      <h2>{pair}</h2>
-      {pairInfo.base ? <p>Base: {pairInfo.base}</p> : null}
-      {pairInfo.date ? <p>Date: {pairInfo.date}</p> : null}
-      {pairInfo.rates && Object.keys(pairInfo.rates).length > 0 ? (
-        <ul>
-          {Object.entries(pairInfo.rates).map(([currency, rate]) => (
-            <li key={currency}>{currency}: {rate}</li>
-          ))}
-        </ul>
-      ) : null}
+    <div className="pair-page-wrapper">
+      <div className="container">
+        <div className="row">
+          <div className="col-12 content-wrapper">
+            <h2 className="pair-name">{pair}</h2>
+            {pairInfo.base ? <p className="base">Base: {pairInfo.base}</p> : null}
+            {pairInfo.date ? <p className="date">Date: {pairInfo.date}</p> : null}
+            {pairInfo.rates && Object.keys(pairInfo.rates).length > 0 ? (
+              <ul className="pair-info">
+                {Object.entries(pairInfo.rates).map(([currency, rate]) => (
+                  <li key={currency}>{currency}: {rate}</li>
+                ))}
+              </ul>
+            ) : null}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
